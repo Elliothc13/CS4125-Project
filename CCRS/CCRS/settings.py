be009111276ad9f13964b905a91b95bf9fdf4f1b
dawidK5 <19233116@studentmail.ul.ie>
2022-10-26 15:21:48 +0100

    Update settings

--- CCRS/CCRS/settings.py
+++ CCRS/CCRS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pc+4ua%nvag(8zdn0n^774vg^6w@#pz152i_ux1$iew-&%$&$l'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
