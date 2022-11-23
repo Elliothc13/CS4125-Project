ccfa1be5d5bb97cea304dc93e61ca270cc2b3d4f
Elliothc13 <19264259@studentmail.ul.ie>
2022-11-23 16:52:45 +0000

    refactor(strategy): :recycle: cleaned up factory.py

--- CCRS/tokens/factory.py
+++ CCRS/tokens/factory.py
@@ -1,22 +1,21 @@
 from abc import ABC, abstractmethod
 from __future__ import annotations
 from typing import List
+
 class UpgradeVolunteerTierCreator(ABC):
     @abstractmethod
     def createTier(level):
         pass
 
+
     def calculateTokens(level, hoursWorked):
         tierProduct = createTier(level)
         return tierProduct.calculateTokens(hoursWorked)
-
 
 class TierFactoryConcreteCreator(UpgradeVolunteerTierCreator):
 # create new, different types of instances of a parent
 # classes have same methods, but each method behaves differently
 # many factories can exist
-# 
-#
     def createTier(self, level):
         if level == 0:
             context = Context(ConcreteStrategy())
@@ -35,39 +34,30 @@ class TierProduct(ABC):
     def calculateTokens(self, hours_worked):
         pass
 
-# class BronzeTierConcreteProduct(TierProduct):
-#     def calculateTokens(self, hoursWorked):
-#         return hoursWorked * 1
-# class SilverTierConcreteProduct(TierProduct):
-#     def calculateTokens(self, hoursWorked):
-#         return hoursWorked * 2
-# class GoldenTierConcreteProduct(TierProduct):
-#     def calculateTokens(self, hoursWorked):
-#         return hoursWorked * 3
-
-
-
-
 class Context():
-
     def __init__(self, strategy: Strategy) -> None:
         self._strategy = strategy
+
 
     @property
     def strategy(self) -> Strategy:
         return self._strategy
 
+
     @strategy.setter
     def strategy(self, strategy: Strategy) -> None:
         self._strategy = strategy
 
+
     def do_some_business_logic(self, hoursWorked, n) -> None:
         result = self._strategy.do_algorithm(hoursWorked, n)
         return result
+
 class Strategy(ABC):
     @abstractmethod
     def do_algorithm(self, hoursWorked, n):
         pass
+
 class ConcreteStrategy(Strategy):
     def do_algorithm(self, hoursWorked, n) -> List:
         return hoursWorked * n
