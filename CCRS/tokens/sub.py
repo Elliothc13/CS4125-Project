6349685b417aaf10224023aaa9f9b3254c00522e
Elliothc13 <19264259@studentmail.ul.ie>
2022-11-24 16:15:35 +0000

    fix(code smells): :wastebasket: cleaning more code smells

--- CCRS/tokens/sub.py
+++ CCRS/tokens/sub.py
@@ -7,22 +7,26 @@ from .models import Event, Volunteer, VolunteerEvent, Organisation
 from .emails import EmailSender
 from .upgrade_tiers import LinkMaker, LinkComponent
 
+
 class Subject(ABC):
-     @abstractmethod
-     def attach(self, observer: observe):
-         pass
-     @abstractmethod
-     def detach(self, observer: observe):
-         pass
-     @abstractmethod
-     def notify(self):
-         pass
+    @abstractmethod
+    def attach(self, observer: observe):
+        pass
+
+    @abstractmethod
+    def detach(self, observer: observe):
+        pass
+
+    @abstractmethod
+    def notify(self):
+        pass
+
 
 class VolunteeringFlow:
     def __init__(self, entryId) -> None:
 
         super().__init__()
-        
+
         self.state = None
         self.entryId = entryId
         self._observers = []
@@ -36,16 +40,19 @@ class VolunteeringFlow:
     def notify(self):
         for observer in self._observers:
             observer.update(self.state, self.entryId)
-    
+
     def setState(self, updatedState):
         self.state = updatedState
         self.notify()
 
 # add abstract before class definition
+
+
 class Observer(ABC):
-     @abstractmethod
-     def update(self, state):
-         pass
+    @abstractmethod
+    def update(self, state):
+        pass
+
 
 class OrganisationNotifier(Observer):
     def __init__(self, organisationId, reactableStates):
@@ -55,14 +62,16 @@ class OrganisationNotifier(Observer):
     def update(self, state, entryId):
         if state in self.reactableStates:
             print("========Notifying organisation a user applied for a position")
-            orgEmail = Organisation.objects.get(userId=self.organisationId).userEmail
-            EmailSender.sendEmail("system@email.com", orgEmail, "User needs approval", "User has requested your approval. To approve click here: " + LinkMaker(LinkComponent('http://127.0.0.1:8000/tokens'), '/entryId/' + str(entryId)).getLink())
+            orgEmail = Organisation.objects.get(
+                userId=self.organisationId).userEmail
+            EmailSender.sendEmail("system@email.com", orgEmail, "User needs approval", "User has requested your approval. To approve click here: " +
+                                  LinkMaker(LinkComponent('http://127.0.0.1:8000/tokens'), '/entryId/' + str(entryId)).getLink())
+
 
 class VolunteerNotifier(Observer):
     def __init__(self, volunteerId, reactableStates):
         self.volunteerId = volunteerId
         self.reactableStates = reactableStates
-
 
     def update(self, state, entryId):
         if state in self.reactableStates:
@@ -77,9 +86,10 @@ class GenerateToken:
         self.organisationId = organisationId
         self.subject = VolunteeringFlow(self.fetchEntryId())
         self.states = VolunteerEvent.UserStates
-        self.subject.attach(OrganisationNotifier(self.organisationId.userId, self.generateOrgStates()))
-        self.subject.attach(VolunteerNotifier(self.volunteerId, self.generateVolStates()))
-    
+        self.subject.attach(OrganisationNotifier(
+            self.organisationId.userId, self.generateOrgStates()))
+        self.subject.attach(VolunteerNotifier(
+            self.volunteerId, self.generateVolStates()))
 
     def generateOrgStates(self):
         return [self.states.USER_APPLIED, self.states.TOKENS_REQUESTED]
@@ -88,10 +98,9 @@ class GenerateToken:
         all_states = VolunteerEvent.UserStates
         return [all_states.USER_ADMITTED, all_states.TOKENS_CLAIMED]
 
-    
     def calculateTokens(self, current_balance, currentTier):
         return current_balance + currentTier
-    
+
     @staticmethod
     def can_generate_token(currentState):
         return GenerateToken.can_request_token(currentState) or GenerateToken.can_claim_token(currentState)
@@ -103,12 +112,12 @@ class GenerateToken:
     @staticmethod
     def can_claim_token(currentState):
         return currentState == VolunteerEvent.UserStates.TOKENS_APPROVED
-    
+
     @staticmethod
     def get_current_tokens(userId):
         print(Volunteer.objects.get(userId=userId).tokenBalance)
         return Volunteer.objects.get(userId=userId).tokenBalance
-    
+
     def fetchState(self):
         return VolunteerEvent.objects.get(userId=self.volunteerId, eventId=int(self.eventId)).state
 
@@ -116,14 +125,16 @@ class GenerateToken:
         return VolunteerEvent.objects.get(userId=self.volunteerId, eventId=int(self.eventId)).id
 
     def request_token(self):
-        entry = VolunteerEvent.objects.get(userId=self.volunteerId, eventId=self.eventId)
+        entry = VolunteerEvent.objects.get(
+            userId=self.volunteerId, eventId=self.eventId)
         updated_state = self.states.TOKENS_REQUESTED
         entry.state = updated_state
         entry.save()
-        self.subject.setState(updated_state) # notifies the organisation
+        self.subject.setState(updated_state)  # notifies the organisation
 
     def claim_token(self):
-        entry = VolunteerEvent.objects.get(userId=self.volunteerId, eventId=self.eventId)
+        entry = VolunteerEvent.objects.get(
+            userId=self.volunteerId, eventId=self.eventId)
         updated_state = self.states.TOKENS_CLAIMED
         entry.state = updated_state
         entry.save()
@@ -133,37 +144,12 @@ class GenerateToken:
         updated_balance = self.calculateTokens(current_balance, current_tier)
         user.tokenBalance = updated_balance
         user.save()
-        self.subject.setState(self.states.TOKENS_CLAIMED) # notifies the user
+        self.subject.setState(self.states.TOKENS_CLAIMED)  # notifies the user
 
-
-    def generateToken(self): # updates tokens
+    def generateToken(self):  # updates tokens
         currentState = self.fetchState()
         if self.can_request_token(currentState):
             self.request_token()
-           
+
         elif self.can_claim_token(currentState):
             self.claim_token()
-
-    # class VolunteeringFlowState:
-    #      def flow_clear():
-    #          pass
-    #      def advance_flow(current):
-    #          pass
-
-    # class TokensRequestedState:
-    #      def flow_clear():
-    #          return True
-    #      def advance_flow():
-    #          if current_State == VolunteerEvent.UserStates.TOKENS_REQUESTED:
-    #              user = Volunteer.objects.get(id=Self.volunteerId)
-    #              current_balance = user.tokenBalance
-    #              updated_balance = Self.calculateTokens(current_balance, currentThread)
-    #              user.tokenBalance = updated_balance
-    #              user.save()
-    #              return True
-    #          return False
-    # class TokensClaimedState:
-    #      def flow_clear():
-    #          return True
-    #      def advance_flow():
-    #          return True
--- CCRS/tokens/views.py
+++ CCRS/tokens/views.py
@@ -7,9 +7,9 @@ from .sub import GenerateToken
 # b = Blog(name='Beatles Blog', tagline='All the latest Beatles news.')
 # b.save() actually inserts
 
+
 def confirmed_events(request):
-    volunteerId=request.user.id
-    # volunteer_entries = VolunteerEvent.objects.filter(userId=volunteerId)
+    volunteerId = request.user.id
     if request.user.is_authenticated:
         if request.method == 'POST':
             eventToUpdate = request.POST.get('eventId')
@@ -20,17 +20,20 @@ def confirmed_events(request):
             service.generateToken()
             return redirect('list-confirmed-events')
         else:
-            volunteer_entries = VolunteerEvent.objects.filter(userId=volunteerId)
+            volunteer_entries = VolunteerEvent.objects.filter(
+                userId=volunteerId)
             events_to_display = []
             print('=1====', volunteer_entries, '=====')
             for entry in volunteer_entries:
                 event_temp = entry.eventId
                 print('+++++', event_temp)
                 event_temp.state = entry.state
-                event_temp.btDisabled = not GenerateToken.can_generate_token(event_temp.state)
-                events_to_display.append(event_temp) # caution, Django returns the model not the field here
+                event_temp.btDisabled = not GenerateToken.can_generate_token(
+                    event_temp.state)
+                # caution, Django returns the model not the field here
+                events_to_display.append(event_temp)
             print('-------', request.user.id)
             tokenBalance = GenerateToken.get_current_tokens(request.user.id)
             return render(request, 'tokens/events_list.html', {'events_list': events_to_display, 'tokenBalance': tokenBalance})
-        
+
     return redirect('list-confirmed-events')
