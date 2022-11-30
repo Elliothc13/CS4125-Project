4aca4f9e06fa8c2212b115cf468ecdbf101ce242
Elliothc13 <19264259@studentmail.ul.ie>
2022-11-30 14:42:54 +0000

    feat: :sparkles: started adding fnctionality to "Express Interest"

---
+++ CCRS/recommender/sub.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from .models import Organisation
+from tokens.emails import EmailSender
+from tokens.upgrade_tiers import LinkMaker, LinkComponent
+
+# add abstract before class definition
+class Observer(ABC):
+    @abstractmethod
+    def update(self, state):
+        pass
+
+class OrganisationNotifier(Observer):
+    def __init__(self, organisationId, reactableStates):
+        self.organisationId = organisationId
+        self.reactableStates = reactableStates
+
+    def update(self, state, entryId):
+        if state in self.reactableStates:
+            print("========Notifying organisation a user applied for a position")
+            orgEmail = Organisation.objects.get(
+                userId=self.organisationId).userEmail
+            EmailSender.sendEmail("system@email.com", orgEmail, "User needs admittion", "User has requested to work with you. To approve click here: " +
+                                  LinkMaker(LinkComponent('http://127.0.0.1:8000/reccomendation'), '/entryId/' + str(entryId)).getLink())
--- CCRS/recommender/views.py
+++ CCRS/recommender/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from tokens.models import Event, Volunteer
-from tokens.sub import GenerateToken
+from tokens.models import Event
+from .sub import OrganisationNotifier
 
 
 def confirmed_events(request):
@@ -10,10 +10,6 @@ def confirmed_events(request):
         if request.method == 'POST':
             eventToUpdate = request.POST.get('eventId')
             print('???????', eventToUpdate)
-            organisationId = Event.objects.get(eventId=eventToUpdate).organiser
-            currentTier = Volunteer.objects.get(userId=volunteerId).currentTier
-            service = GenerateToken(volunteerId, eventToUpdate, organisationId)
-            service.generateToken()
             return redirect('list-recommendations')
         else:
             #default and what the home button will do
@@ -26,7 +22,6 @@ def confirmed_events(request):
                 # caution, Django returns the model not the field here
                 events_to_display.append(event_temp)
             print('-------', request.user.id)
-            tokenBalance = GenerateToken.get_current_tokens(request.user.id)
-            return render(request, 'recommender/recommendations_list.html', {'events_list': events_to_display, 'tokenBalance': tokenBalance})
+            return render(request, 'recommender/recommendations_list.html', {'events_list': events_to_display})
 
     return redirect('list-recommendations')
